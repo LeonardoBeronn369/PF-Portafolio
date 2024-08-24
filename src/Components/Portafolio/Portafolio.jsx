08c4f2b26d6403f2c10b6872414295dda538b22c
leonardo.beron <leonardoberon33@gmail.com>
2024-08-24 03:08:16 -0300

    arregle imagenes con mejores estilos

--- src/Components/Portafolio/Portafolio.jsx
+++ src/Components/Portafolio/Portafolio.jsx
@@ -4,8 +4,8 @@ import './Portafolio.css'
 
 const Portafolio = () => {
   return (
-    <div className='page'>
-      <h1 className='heading'>Portafolio</h1>
+    <div className='contenedor-img'>
+      <h1 className='heading-portafolio'>Portafolio</h1>
       <h2 className='text-portafolio'>Todo lo que puedas <strong>Imaginar</strong> , lo podras <strong>programar</strong>.</h2>
       <ListadoTrabajos/>
     </div>
